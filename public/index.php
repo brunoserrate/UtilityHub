c51132ab0966f764bc94a72f6a4632f7662a8d98
brunoserrate <brunostj@hotmail.com>
2024-04-27 21:37:07 -0300

    #9 - feat: added template functionality, app can now render partially using a template;refactor: move some folders and files;

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
+define("ROOT_PATH", __DIR__ . DIRECTORY_SEPARATOR . ".." );
 
 require join(DIRECTORY_SEPARATOR, [__DIR__, '..','vendor' ,'autoload.php']);
+
+use App\Utils\Helpers\EnvLoader;
+
+define("ENV", EnvLoader::loadEnv());
 
 $router = require join(DIRECTORY_SEPARATOR, [__DIR__,'..','src','app','Routes','index.php']);

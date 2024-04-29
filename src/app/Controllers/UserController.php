3acd7cb5ee22abce73400d1f874d58c5a5aca109
brunoserrate <brunostj@hotmail.com>
2024-04-29 17:41:35 -0300

    #11 - fix: fixed usercontroller __construct problem

--- src/app/Controllers/UserController.php
+++ src/app/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller {
 
     private $userRepository;
 
-    protected function __construct() {
+    public function __construct() {
         $this->userRepository = new UserRepository();
     }
 

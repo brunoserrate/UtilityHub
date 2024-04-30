0a3de75638f015a3675ca7d628cb79d268273b8d
brunoserrate <brunostj@hotmail.com>
2024-04-30 00:49:49 -0300

    #13 - feat: added js packages

--- src/app/Controller.php
+++ src/app/Controller.php
@@ -6,7 +6,7 @@ class Controller {
     protected $template = "clean_default";
     protected $cdns = [
         'css' => ['css/custom.css'],
-        'js' => []
+        'js' => ['../node_modules/jquery/dist/jquery.js', '../node_modules/bootstrap/dist/js/bootstrap.bundle.js']
     ];
 
     protected $title = ENV['APP_NAME'];

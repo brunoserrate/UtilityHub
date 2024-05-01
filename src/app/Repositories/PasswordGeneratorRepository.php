c35b5914a45e31352f8fcebc5774de15c1f43850
brunoserrate <brunostj@hotmail.com>
2024-05-01 14:15:09 -0300

    #15 - feat: added generate password function to the repository

--- src/app/Repositories/PasswordGeneratorRepository.php
+++ src/app/Repositories/PasswordGeneratorRepository.php
@@ -2,7 +2,31 @@
 namespace App\Repositories;
 
 use App\Repositories\BaseRepository;
+use App\Utils\Helpers\PasswordGeneratorHelper;
 
 class PasswordGeneratorRepository extends BaseRepository {
+
+    public function generate($params) {
+        $passwordGeneratorHelper = new PasswordGeneratorHelper();
+
+        $length = isset($params['length']) ? $params['length'] : 8;
+        $samples = isset($params['samples']) ? $params['samples'] : 1;
+        $useLowerCase = isset($params['useLowerCase']) ? $params['useLowerCase'] : true;
+        $useUpperCase = isset($params['useUpperCase']) ? $params['useUpperCase'] : true;
+        $useNumbers = isset($params['useNumbers']) ? $params['useNumbers'] : true;
+        $useSymbols = isset($params['useSymbols']) ? $params['useSymbols'] : false;
+        $useSimilarCharacters = isset($params['useSimilarCharacters']) ? $params['useSimilarCharacters'] : false;
+        $uniqueCharacters = isset($params['uniqueCharacters']) ? $params['uniqueCharacters'] : false;
+
+        try {
+            $passwords = $passwordGeneratorHelper->generatePassword($length, $samples, $useLowerCase, $useUpperCase, $useNumbers, $useSymbols, $useSimilarCharacters, $uniqueCharacters);
+
+            return $this->sendSuccess('Password generated successfully', $passwords);
+        } catch (\Throwable $th) {
+            return $this->sendError('Error generating password');
+        }
+
+
+    }
 
 }
